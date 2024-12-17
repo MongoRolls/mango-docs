de0f1bd945d743760ab2e8d75c738b7652196d3f
xzc <xuzhichao1618@qq.com>
2024-12-17 19:12:48 +0800

    feat: 增加状态管理zustand

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -2,14 +2,15 @@ interface DocumentIdPageProps {
   params: Promise<{ documentId: string }>;
 }
 
-import { Editor } from "@/app/documents/editor";
-
+import { Editor } from '@/app/documents/[documentId]/editor';
+import { Toolbar } from '@/app/documents/[documentId]/toolbar';
 const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { documentId } = await params;
 
   return (
     <div className="min-h-screen bg-[#FAFBFD]">
-      DocumentIdPage: {documentId}
+      <Toolbar />
       <Editor />
     </div>
   );
